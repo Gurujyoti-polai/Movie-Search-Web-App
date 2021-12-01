f933c930073b361d0d3df2f796f0b5206af00c6f
Gurujyoti polai <1905881@kiit.ac.in>
2021-12-01 18:40:55 +0530

    adding search result in the store

--- src/index.js
+++ src/index.js
@@ -1,45 +1,65 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
 import App from './components/App';
-
-import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './reducers';
+import './index.css';
 
-//function logger(obk,next, action)
-//logger(obj)(next)(action) 
 // const logger = function({ dispatch, getState }) {
 //   return function(next) {
 //     return function(action) {
 //       // my middlware
-//       console.log('ACTION_TYPE', action.type);
+//       console.log('ACTION', action);
 //       next(action);
 //     };
 //   };
 // };
 
 const logger = ({ dispatch, getState }) => (next) => (action) => {
-  // logger code
+  // my middlware
   console.log('ACTION', action);
   next(action);
 };
 
-const store = createStore(rootReducer, applyMiddleware(logger));
-console.log('store',store);
-// console.log('BEFORE STATE',store.getState());
+// const thunk = store => next => action => {
+//   if (typeof action === 'function') {
+//     return action(store.dispatch);
+//   }
 
+//   next(action);
+// };
+
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+// console.log(store);
+console.log('state', store.getState());
+
+export const StoreContext = createContext();
+
+console.log('StoreContext', StoreContext);
+
+class Provider extends React.Component {
+  render() {
+    const { store } = this.props;
+    return (
+      <StoreContext.Provider value={store}>
+        {this.props.children}
+      </StoreContext.Provider>
+    );
+  }
+}
+
+// update store by dispatching actions
 // store.dispatch({
-//   type: 'ADD_movies',
-//   movies: [{name:'Superman'}]
-// })
-
-// console.log('AFTER STATE',store.getState());
+//   type: 'ADD_MOVIES',
+//   movies: moviesList
+// });
+// console.log('state', store.getState());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App store={store} />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
-
-
